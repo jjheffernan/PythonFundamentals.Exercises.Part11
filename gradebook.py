4959cc40e7c8465345e61b8da144f0da653aa88e
jjheffernan <jjheffernan99@gmail.com>
2022-07-18 04:46:13 -0400

    added in subclass functionality of Students and Instructors.

--- gradebook.py
+++ gradebook.py
@@ -8,34 +8,68 @@ class AliveStatus(Enum):
 
 
 class Person:
-    def __init__(self):
-        person = Person()
-        firstname = ''
-        lastname = ''
-        dob = ''
+    def __init__(self, first_name, last_name, dob, status):
+        self.person_arr = {
+            'firstname': first_name,
+            'lastname': last_name,
+            'dateofbirth': dob,
+            'status': AliveStatus.Alive
+                      }
+        # person = Person()
+        # firstname = ''
+        # lastname = ''
+        # dob = ''
+        # is_alive = AliveStatus.Alive
 
-    def update_first_name(self):
-        pass
+    def update_first_name(self, first_name):
+        new_name = {'firstname': first_name}
+        self.person_arr.update(new_name)
+        return self.person_arr
+        # pass # escape pass
 
-    def update_last_name(self):
-        pass
+    def update_last_name(self, last_name):
+        new_name = {'lastname': last_name}
+        self.person_arr.update(new_name)
+        return self.person_arr
+        # pass
 
-    def update_dob(self):
-        pass
+    def update_dob(self, new_date):
+        new_dob = {'dateofbirth': new_date}
+        self.person_arr.update(new_dob)
+        return self.person_arr
+        # pass # escape return
 
-    def update_status(self):
-        pass
+    def update_status(self, new_status):
+        event = {'status': new_status}
+        self.person_arr.update(event)
+        return self.person_arr
+        # pass
 
 
-class Instructor(Person):
-    def __init__(self):
-        instructor_id = "Instructor_", 0
+class PersonMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class Instructor(Person, PersonMixin):
+    def __init__(self, instructor_id, first_name, last_name, dob, status):
+        super().__init__(first_name, last_name, dob, status)
+        person_arr = super().person_arr
+        teach_id = {'instructor_id': instructor_id}
+        person_arr.update(teach_id)
+        # self, first_name, last_name , dob , status
+
+        # instructor_id = "Instructor_", 0
     # need to implement super class calls
 
 
 class Student(Person):
-    def __init__(self):
-        student_id = "Student_", 0
+    def __init__(self, s_id, first_name, last_name, dob, status):
+        super().__init__(first_name, last_name, dob, status)
+        person_arr = super().person_arr
+        student_id = {'Student_': s_id}
+        person_arr.update(student_id)
+        # student_id = "Student_", 0
 
 
 class ZipCodeStudent(Student):
@@ -43,8 +77,12 @@ class ZipCodeStudent(Student):
 
 
 class CollegeStudent(Student):
-    def __init__(self):
-        college_name = "University"
+    def __init__(self, college, first_name, last_name, dob, status):
+        super().__init__(first_name, last_name, dob, status)
+        person_arr = super().person_arr
+        college_name = {'university': college}
+        person_arr.update(college_name)
+        # college_name = "University"
 
 
 class Classroom:
