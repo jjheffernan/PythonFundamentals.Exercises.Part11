e042289923531348190eaecd5f18e11509cbdde0
jjheffernan <jjheffernan99@gmail.com>
2022-07-29 14:48:23 -0400

    refactored tests. 16 tests, 11 pass Must improve test difficulty.

--- test_gradebook.py
+++ test_gradebook.py
@@ -11,7 +11,7 @@ Use the below fileReader Test class to work develop framework for
 
 class GradebookTest(unittest.TestCase):
 
-    @patch('sys.stdout', new_callable=StringIO)
+    # @patch('sys.stdout', new_callable=StringIO)
     def test_create_person(self, mock_stdout):
         test_cases = [
             (
@@ -33,96 +33,164 @@ class GradebookTest(unittest.TestCase):
                 # self.assertEqual(expected, actual)
 
     def test_update_Person_first_name(self):
-        test_cases = [
-            ("locked_out_file.txt", PermissionError),
-            ("file_that_does_not_exist.txt", FileNotFoundError)
-        ]
-        for file_path, expected in test_cases:
-            with self.subTest(f"{file_path}"):
-                with self.assertRaises(expected):
-                    file_reader.reckless_file_reader(file_path)
+        def test_create_person(self, mock_stdout):
+            test_cases = [
+                (
+                    (),
+                    (),
+                    (),
+                    ()
+                )
+            ]  # updated with tuples for names
+            for test_person, expected in test_cases:
+                with self.subTest(f"{test_person}"):
+                    expected = {}  # person array of person details
+
+                    actual = gradebook.Person(test_cases[test_person])
+                    self.assertEqual(expected, actual)
 
     # @unittest.skip("Useless test. This exists simply to trigger and demo the function under test.")
     def test_update_Person_last_name(self):
-        with self.assertRaises(PermissionError):
-            file_reader.quick_way_to_get_fired("locked_out_file.txt")
+        def test_create_person(self, mock_stdout):
+            test_cases = [
+                (
+                    (),
+                    (),
+                    (),
+                    ()
+                )
+            ]  # updated with tuples for names
+            for test_person, expected in test_cases:
+                with self.subTest(f"{test_person}"):
+                    expected = {}  # person array of person details
+
+                    actual = gradebook.Person(test_cases[test_person])
+                    self.assertEqual(expected, actual)
 
     def test_update_Student_dob(self):
-        with self.assertLogs() as cm:
-            file_reader.single_exception_handling_reader("file_that_does_not_exist.txt")
-            self.assertIn("No such file or directory: 'file_that_does_not_exist.txt'", cm.output[0])
+        def test_create_person(self, mock_stdout):
+            test_cases = [
+                (
+                    (),
+                    (),
+                    (),
+                    ()
+                )
+            ]  # updated with tuples for names
+            for test_person, expected in test_cases:
+                with self.subTest(f"{test_person}"):
+                    expected = {}  # person array of person details
+
+                    actual = gradebook.Person(test_cases[test_person])
+                    self.assertEqual(expected, actual)
 
     def test_update_Teacher_status(self):
-        with self.assertRaises(PermissionError):
-            file_reader.single_exception_handling_reader("locked_out_file.txt")
+        def test_create_person(self, mock_stdout):
+            test_cases = [
+                (
+                    (),
+                    (),
+                    (),
+                    ()
+                )
+            ]  # updated with tuples for names
+            for test_person, expected in test_cases:
+                with self.subTest(f"{test_person}"):
+                    expected = {}  # person array of person details
+
+                    actual = gradebook.Person(test_cases[test_person])
+                    self.assertEqual(expected, actual)
 
     def test_null_constructor_mixin(self):
-        test_cases = [
-            (
-                "file_that_does_not_exist.txt",
-                "No such file or directory: 'file_that_does_not_exist.txt'"
-            ),
-            (
-                "locked_out_file.txt",
-                "Permission denied: 'locked_out_file.txt'"
-            )
-        ]
-        for file_path, message in test_cases:
-            with self.subTest(f"{file_path} -> {message}"):
-                with self.assertLogs() as cm:
-                    file_reader.multiple_exception_handling_reader(file_path)
-                    self.assertIn(message, cm.output[0])
+        def test_create_person(self, mock_stdout):
+            test_cases = [
+                (
+                    (),
+                    (),
+                    (),
+                    ()
+                )
+            ]  # updated with tuples for names
+            for test_person, expected in test_cases:
+                with self.subTest(f"{test_person}"):
+                    expected = {}  # person array of person details
 
-    @patch('sys.stdout', new_callable=StringIO)
+                    actual = gradebook.Person(test_cases[test_person])
+                    self.assertEqual(expected, actual)
+
+    # @patch('sys.stdout', new_callable=StringIO)
     def test_create_Instructor(self, mock_stdout):
-        test_cases = [
-            "locked_out_file.txt",
-            "file_that_does_not_exist.txt"
-        ]
-        for file_path in test_cases:
-            with self.subTest(f"{file_path}"):
-                file_reader.base_class_exception_handling_reader(file_path)
-                self.assertIn(
-                    "Error opening the file. Please ensure the file exists and has appropriate permissions.",
-                    mock_stdout.getvalue()
+        def test_create_person(self, mock_stdout):
+            test_cases = [
+                (
+                    (),
+                    (),
+                    (),
+                    ()
                 )
+            ]  # updated with tuples for names
+            for test_person, expected in test_cases:
+                with self.subTest(f"{test_person}"):
+                    expected = {}  # person array of person details
 
-    @patch('sys.stdout', new_callable=StringIO)
+                    actual = gradebook.Person(test_cases[test_person])
+                    self.assertEqual(expected, actual)
+
+    # @patch('sys.stdout', new_callable=StringIO)
     def test_create_Zipcoder(self, mock_stdout):
-        test_cases = [
-            "locked_out_file.txt",
-            "file_that_does_not_exist.txt"
-        ]
-        for file_path in test_cases:
-            with self.subTest(f"{file_path}"):
-                file_reader.tuple_exception_handling_reader(file_path)
-                self.assertIn(
-                    "Error opening the file. Please ensure the file exists and has appropriate permissions.",
-                    mock_stdout.getvalue()
+        def test_create_person(self, mock_stdout):
+            test_cases = [
+                (
+                    (),
+                    (),
+                    (),
+                    ()
                 )
+            ]  # updated with tuples for names
+            for test_person, expected in test_cases:
+                with self.subTest(f"{test_person}"):
+                    expected = {}  # person array of person details
 
-    @patch('sys.stdout', new_callable=StringIO)
+                    actual = gradebook.Person(test_cases[test_person])
+                    self.assertEqual(expected, actual)
+
+    # @patch('sys.stdout', new_callable=StringIO)
     def test_test_create_college_student(self, mock_stdout):
-        test_cases = [
-            "locked_out_file.txt",
-            "file_that_does_not_exist.txt"
-        ]
-        for file_path in test_cases:
-            with self.subTest(f"{file_path}"):
-                file_reader.better_file_reader(file_path)
-                self.assertIn(
-                    "Error opening the file. Please ensure the file exists and has appropriate permissions.",
-                    mock_stdout.getvalue()
+        def test_create_person(self, mock_stdout):
+            test_cases = [
+                (
+                    (),
+                    (),
+                    (),
+                    ()
                 )
+            ]  # updated with tuples for names
+            for test_person, expected in test_cases:
+                with self.subTest(f"{test_person}"):
+                    expected = {}  # person array of person details
+
+                    actual = gradebook.Person(test_cases[test_person])
+                    self.assertEqual(expected, actual)
+
+
+class ClassRoomTest(unittest.TestCase):
 
     def test_create_classrom(self):
         test_classroom = gradebook.Classroom()
-
-        with patch('file_reader.process_file') as cm:
-            file_reader.better_file_reader("my_awesome_file.txt")
-        cm.assert_called_once()
+        test_cases = [
+            (
+                (),
+                (),
+                (),
+                ()
+            )
+        ]  # updated with tuples for names
+        expected = test_cases
+        actual = test_classroom
+        self.assertEqual(expected, actual)
 
     def test_add_instructor(self):
+
         pass
 
     def test_remove_instructor(self):
